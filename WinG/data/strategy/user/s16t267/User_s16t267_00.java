46d6fdf1515a7608dfc81ebbd66dafc90f8bc2da
furu8 <foooolurk1125@gmail.com>
2019-02-17 01:42:59 +0900

    飛び関数の修正

--- WinG/data/strategy/user/s16t267/User_s16t267_00.java
+++ WinG/data/strategy/user/s16t267/User_s16t267_00.java
@@ -79,7 +79,7 @@ public class User_s16t267_00 extends GogoCompSub {
         int [][] cell = board.get_cell_all();   // 盤面情報
         int mycolor;                            // 自分の石の色
         mycolor = role;                         // プレイヤーの色　-1or1
-        
+
         //--  各マスの評価値
         for ( int i = 0; i < size; i++ ) {
             for ( int j = 0; j < size; j++ ) {
@@ -96,71 +96,75 @@ public class User_s16t267_00 extends GogoCompSub {
                 }
                 
                 // 相手の五連を崩す → 1000;
-                if ( check_run(cell, mycolor*-1, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 5) ) {
                     if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 1000; }
                     values[i][j] = 1000;
                     continue;
                 }
 
                 // 勝利(五取) → 950;
-                // if ( check_rem(cell, mycolor*-1, i, j) ) {
+                // if ( check_rem(cell, values, mycolor*-1, i, j) ) {
                 //     values[i][j] = 950;
                 //     continue;
                 // }
                 // 勝利(五連) → 900;
-                if ( check_run(cell, mycolor, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor, i, j, 5) ) {
                     values[i][j] = 900;
                     continue;
                 }
                 // 敗北阻止(五取) → 850;
-                // if ( check_rem(cell, mycolor, i, j) )  {
+                // if ( check_rem(cell, values,  mycolor, i, j) )  {
                 //     values[i][j] = 850;
                 //     continue;
                 // }
                 // 敗北阻止(五連) → 800;
-                if ( check_run(cell, mycolor*-1, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 5) ) {
                     values[i][j] = 800;
                     continue;
                 }
                 // 相手の四連を止める → 700;
-                if ( check_run(cell, mycolor*-1, i, j, 4) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 4) ) {
                     values[i][j] = 700;
                     continue;
                 }
                 // 自分の四連を作る → 600;
-                if ( check_run(cell, mycolor, i, j, 4) ) {
+                if ( check_run(cell, values, mycolor, i, j, 4) ) {
                     values[i][j] = 600;
                     continue;
                 }
-                // 相手の三連を止める → 500;
-                if ( check_run(cell, mycolor*-1, i, j, 3) ) {
+                // 自分の三連または四連を作る（飛び）→ 500
+                 if ( check_run_tobi(cell, mycolor, i, j) ) {
                     values[i][j] = 500;
                     continue;
                 }
-                // 相手の石の間に置く → 400
-                if ( values[i][j] == 0 ) {
-                    near_random(cell, values, mycolor*-1, i, j);
+                // 自分の三連を作る → 400;
+                if ( check_run(cell, values, mycolor, i, j, 3) ) { values[i][j] = 400; }
+                
+                // 相手の石を取る → 350;
+                if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 350; }
+               
+                // 相手の三連を止める → 300;
+                if ( check_run(cell, values, mycolor*-1, i, j, 3) ) { values[i][j] = 300; }
+                
+                // 相手の石の間に置く → 200
+                if ( values[i][j] == 0 && check_run_tobi(cell, mycolor*-1, i, j) ) {
+                    values[i][j] = 200;
                 }
-                // 自分の三連を作る → 300;
-                if ( check_run(cell, mycolor, i, j, 3) ) { values[i][j] = 300; }
                 // 三々の禁じ手は打たない → -1
-                // 相手の石を取る → 300;
-                if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 200; }
-                // 自分の石を守る → 200;
+                // 自分の石を守る → 100;
                 if ( check_rem(cell, mycolor*-1, i, j) ) { values[i][j] = 100; }
                 // ランダム
-                // if ( values[i][j] == 0) {
-                //     int aaa = (int) Math.round(Math.random() * 15);
-                //     //System.out.printf("%d ", aaa);
-                //     if ( values[i][j] < aaa) { values[i][j] = aaa; }
-                // }
+                if ( values[i][j] == 0) {
+                    int aaa = (int) Math.round(Math.random() * 15);
+                    //System.out.printf("%d ", aaa);
+                    if ( values[i][j] < aaa) { values[i][j] = aaa; }
+                }
                 
                 // 四々や四三の判定
                 // 飛び三や飛び四の判定
                 // 三をどちらで止めるか
             }
         }
-        System.out.printf("%d\n", prev.turn);
         output(values);
     }
 
@@ -179,7 +183,7 @@ public class User_s16t267_00 extends GogoCompSub {
                 if ( values[i][j] == -2 ) { continue; }
                 //--  適当な評価の例
                 // 相手の五連を崩す → 1000;
-                if ( check_run(cell, mycolor*-1, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 5) ) {
                     if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 1000; }
                     values[i][j] = 1000;
                     continue;
@@ -191,7 +195,7 @@ public class User_s16t267_00 extends GogoCompSub {
                 //     continue;
                 // }
                 // 勝利(五連) → 900;
-                if ( check_run(cell, mycolor, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor, i, j, 5) ) {
                     values[i][j] = 900;
                     continue;
                 }
@@ -201,42 +205,45 @@ public class User_s16t267_00 extends GogoCompSub {
                 //     continue;
                 // }
                 // 敗北阻止(五連) → 800;
-                if ( check_run(cell, mycolor*-1, i, j, 5) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 5) ) {
                     values[i][j] = 800;
                     continue;
                 }
                 // 相手の四連を止める → 700;
-                if ( check_run(cell, mycolor*-1, i, j, 4) ) {
+                if ( check_run(cell, values, mycolor*-1, i, j, 4) ) {
                     values[i][j] = 700;
                     continue;
                 }
                 // 自分の四連を作る → 600;
-                if ( check_run(cell, mycolor, i, j, 4) ) {
+                if ( check_run(cell, values, mycolor, i, j, 4) ) {
                     values[i][j] = 600;
                     continue;
                 }
-                // 相手の三連を止める → 500;
-                if ( check_run(cell, mycolor*-1, i, j, 3) ) {
+                // 自分の三連または四連を作る（飛び）→ 500
+                 if ( check_run_tobi(cell, mycolor, i, j) ) {
                     values[i][j] = 500;
-                    continue;
                 }
-                // 相手の石の間に置く → 400
-                if ( values[i][j] == 0 ) {
-                    near_random(cell, values, mycolor*-1, i, j);
+                // 自分の三連を作る → 400;
+                if ( check_run(cell, values, mycolor, i, j, 3) ) { values[i][j] = 400; }
+                
+                // 相手の石を取る → 350;
+                if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 350; }
+               
+                // 相手の三連を止める → 300;
+                if ( check_run(cell, values, mycolor*-1, i, j, 3) ) { values[i][j] = 300; }
+                
+                // 相手の石の間に置く → 200
+                if ( values[i][j] == 0 && check_run_tobi(cell, mycolor*-1, i, j) ) {
+                    values[i][j] = 200;
                 }
-                // 自分の三連を作る → 300;
-                if ( check_run(cell, mycolor, i, j, 3) ) { values[i][j] = 300; }
                 // 三々の禁じ手は打たない → -1
-                // 相手の石を取る → 300;
-                if ( check_rem(cell, mycolor, i, j) ) { values[i][j] = 200; }
-                // 自分の石を守る → 200;
+                // 自分の石を守る → 100;
                 if ( check_rem(cell, mycolor*-1, i, j) ) { values[i][j] = 100; }
-                // ランダム
-                if ( values[i][j] == 0) {
-                    int aaa = (int) Math.round(Math.random() * 15);
-                    //System.out.printf("%d ", aaa);
-                    if ( values[i][j] < aaa) { values[i][j] = aaa; }
-                }
+                // if ( values[i][j] == 0) {
+                //     int aaa = (int) Math.round(Math.random() * 15);
+                //     //System.out.printf("%d ", aaa);
+                //     if ( values[i][j] < aaa) { values[i][j] = aaa; }
+                // }
                 
                 // 四々や四三の判定
                 // 飛び三や飛び四の判定
@@ -280,69 +287,63 @@ public class User_s16t267_00 extends GogoCompSub {
     }
 
 //----------------------------------------------------------------
-//  相手の石の間に置く
-//----------------------------------------------------------------
-
-    void near_random(int[][] board, int[][] values, int color, int x, int y) {
-        int x1, x2, y1, y2;
-        
-        for ( int k = 0; k < 4; k++ ) {
-            if ( x-1 >= 0 || y-1 >= 0 || x+1 < size || y+1 < size ) { // 盤面内に収まっている
-                change(x, y, x1, x2, y1, y2, k);
-                if ( near(board, color, x1, x2, y1, y2) ) {
-                    values[x][y] = 400;
-                } 
-            }   
-        }
-    } 
-
-    void change(int x, int y, int x1, int x2, int y1, int y2, int n) {
-        if ( n == 0 ) {
-            x1 = x - 1;     x2 = x + 1;
-            y1 = y;         y2 = y;
-        } else if ( n == 1 ) {
-            x1 = x;         x2 = x;
-            y1 = y - 1;     y2 = y + 1;
-        } else if ( n == 2 ) {
-            x1 = x - 1;     x2 = x + 1;
-            y1 = y - 1;     y2 = y + 1;
-        } else if ( n == 3 ) {
-            x1 = x - 1;     x2 = x + 1;
-            y1 = y + 1;     y2 = y - 1;
-        }
-    }
-
-    boolean near(int[][] board, int color, int x1, int x2, int y1, int y2) {
-        if ( board[x1][y1] == color && board[x2][y2] == color ) {
-            return true;
-        }
-        return false;
-    }
-
-//----------------------------------------------------------------
 //  連の全周チェック
 //----------------------------------------------------------------
 
-    boolean check_run(int[][] board, int color, int i, int j, int len) {
+    boolean check_run(int[][] board, int[][] values, int color, int i, int j, int len) {
         for ( int dx = -1; dx <= 1; dx++ ) {
             for ( int dy = -1; dy <= 1; dy++ ) {
                 if ( dx == 0 && dy == 0 ) { continue; }
-                if ( check_run_dir(board, color, i, j, dx, dy, len) ) { return true; }
+                if ( check_run_dir(board, values, color, i, j, dx, dy, len) ) { return true; }
             }
         }
         return false;
     }
 
 //----------------------------------------------------------------
+//  連の飛びチェック（相手の石の間にも置ける）
+//----------------------------------------------------------------
+
+boolean check_run_tobi(int[][] board, int color, int x, int y) {
+    int x1, x2, y1, y2;
+    
+    x1 = x2 = y1 = y2 = 0;
+    if ( x <= 0 || y <= 0 || x >= size-1 || y >= size-1 ) { 
+        return false;
+    } else {                                            // 盤面内に収まっている
+        for ( int k = 0; k < 4; k++ ) {    
+            if ( k == 0 ) {
+                x1 = x - 1;     x2 = x + 1;
+                y1 = y;         y2 = y;
+            } else if ( k == 1 ) {
+                x1 = x;         x2 = x;
+                y1 = y - 1;     y2 = y + 1;
+            } else if ( k == 2 ) {
+                x1 = x - 1;     x2 = x + 1;
+                y1 = y - 1;     y2 = y + 1;
+            } else if ( k == 3 ) {
+                x1 = x - 1;     x2 = x + 1;
+                y1 = y + 1;     y2 = y - 1;
+            }
+            if ( board[x1][y1] == color && board[x2][y2] == color ) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+//----------------------------------------------------------------
 //  連の方向チェック(止連・端連・長連も含む、飛びは無視)
 //----------------------------------------------------------------
 
-    boolean check_run_dir(int[][] board, int color, int i, int j, int dx, int dy, int len) {
+    boolean check_run_dir(int[][] board, int[][] values, int color, int i, int j, int dx, int dy, int len) {
+        //if ( len == 3 ) { len++; }          // 三連の反対側に自分の石があるかないか調べる
         for ( int k = 1; k < len; k++ ) {
             int x = i+k*dx;
             int y = j+k*dy;
             if ( x < 0 || y < 0 || x >= size || y >= size ) { return false; }   // 盤面外
-            if ( board[x][y] != color ) { return false; }                       // プレイヤーの色と一致してない
+            if ( board[x][y] != color ) { return false; }                       // プレイヤーの色と一致してないまたは石がない
         }
         return true;
     }
